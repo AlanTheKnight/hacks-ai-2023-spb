09c4aabcd86799c74ec51f2e4556f9d348b52e66
Artem <artem00561@gmail.com>
2023-08-26 18:49:15 +0300

    update result logic

--- backend/backend/api/serializers.py
+++ backend/backend/api/serializers.py
@@ -12,15 +12,15 @@ class ResultSerializer(serializers.ModelSerializer):
 
 class PresentationSerializer(serializers.ModelSerializer):
     creator = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    result = ResultSerializer(required=True)
+    # result = ResultSerializer(required=True)
 
     class Meta:
         model = Presentation
         fields = '__all__'
 
     def create(self, validated_data):
-        result_data = validated_data.pop('result')
+        # result_data = validated_data.pop('result')
         presentation = Presentation.objects.create(**validated_data)
-        result_data['presentation'] = presentation
+        result_data = {"presentation": presentation}
         result = ResultSerializer.create(ResultSerializer(), result_data)
         return presentation
